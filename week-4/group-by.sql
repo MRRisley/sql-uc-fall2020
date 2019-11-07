4c26cf04885f3beb9f97bce685f4a26508cc37a3
Matt Risley <35645770+MRRisley@users.noreply.github.com>
2019-11-07 10:13:19 -0500

    Update group-by.sql

--- week-4/group-by.sql
+++ week-4/group-by.sql
@@ -1,4 +1,4 @@
-/* WHERE Clause */
+/* GROUP BY Clause */
 
 	/* Recall that: 
 	1. The WHERE filters records using logic.
@@ -7,165 +7,87 @@
            is not required in the ORDER BY.
 	*/
 
-/* 
-NOTE: there are many uses and examples of logic not shown here. But, these examples
-cover roughly 85% of the fundamentals of my work queries. 
-*/
 
 
+/* Aggregation without grouping.
 
-/* WHERE with equivalance logic.
-
-1. How many records exist in the [Person].[Person] table where the FirstName field is Aaron?
-2. How many people received the email promotion coded 2 in the table?
-3. What is the alphabetically first last name that received an email promotion coded 0?
+1. How many records are in the table?
+2. How many records are in the table that were modified after June 1, 2014?
+3. How many records have a value of 'Cincinnati' for the field City?
 
 */
 
-	/* Ex 1 */
-	select * 
-	from adventureworks2017.person.person
-	where firstname = 'aaron'
+select count(*) as record_count from person.[address]
 
-		/* Notes:
+select count(*) as record_count 
+from person.[address]
+where modifieddate >= '6/1/2014'
 
-		(1) WHERE clause always comes after the FROM clause.
-		(2) SQL is not case sensitive by default.
-		(3) Strings, or character values, are always enclosed in single quotations.
-
-		*/
-
-	/* Ex 2 */
-	select * 
-	from adventureworks2017.person.person
-	where emailpromotion = 2
-	--Will the query work if we treat the number 2 as a string?
-
-	/* Ex 3 */
-	select * 
-	from adventureworks2017.person.person
-	where emailpromotion = 0
-	--Where should we put the ORDER BY?
+select count(*) as record_count 
+from person.[address]
+where city='Cincinnati'
 
 
+/* Aggregation with grouping by one item.
 
-
-
-/* WHERE with compound logic.
-
-1. How many people with a first name of Michael received the email promotion coded 2 in the table?
-2. How many people with a first name of Michael or Mark received the email promotion coded 2 in the table? 
+1. How many records exist for each StateProvinceID? Sort by StateProvinceID in ascending order.
+2. What value of StateProvinceID has the most number of records?
+3. How many records exist for each City with StateProvinceID of 14? Order by City in alphabetical order.
 
 */
 
-	/* Ex 1 */
-	select * 
-	from adventureworks2017.person.person
-	where emailpromotion = 2
-	and firstname = 'michael'
-	--AND means that both conditions must be True.
+select stateprovinceid, count(*) as record_count 
+from person.[address]
+group by stateprovinceid
+order by stateprovinceid
 
-	/* Ex 2 */
-	select * 
-	from adventureworks2017.person.person
-	where emailpromotion = 2
-	and firstname = 'michael'
-	or firstname = 'mark'
-	--OR means that at least one condition must be True.
+select stateprovinceid, count(*) as record_count 
+from person.[address]
+group by stateprovinceid
+--order by --complete
 
-	select * 
-	from adventureworks2017.person.person
-	where firstname = 'michael'
-	or firstname = 'mark'
-	and emailpromotion = 2
-	--Does order matter?
-
-	select * 
-	from adventureworks2017.person.person
-	where (firstname = 'michael'
-	or firstname = 'mark')
-	and emailpromotion = 2
-	--ALWAYS put quotations around your OR logic!
-	--Go through each example to explain why all 3 return something different.
-	--This becomes VERY important with NULL values (more later).
-
-	select * 
-	from adventureworks2017.person.person
-	where firstname in ('michael', 'mark')
-	and emailpromotion = 2
-	--IN keyword can be a more straightforward solution for most OR logic.
+select city, count(*) as record_count 
+from person.[address]
+--where --complete
+group by city
+--order by --complete
+--(1) What do we do if we want to include the StateProvinceID as part of the select list?
+--(2) Would it make sense to include AddressLine1 in the query?
 
 
 
 
+/* Aggregation with grouping by multiple items.
 
-/* WHERE with non-equivalance logic.
-
-1. How many records exist in the [Person].[Person] table where the FirstName field is not Aaron?
-2. How many people did not receive the email promotion coded 2 in the table?
-3. How many people received the email promotion coded 2 in the table who were not named Michael or Mark? 
+1. How many records exist for each StateProvinceID and City? Sort by StateProvinceID, then City in ascending order.
+2. What value of City has the most number of records?
+3. What value of StateProvinceID and City has the most number of records?
 
 */
 
-	/* Ex 1 */
-	select * 
-	from adventureworks2017.person.person
-	where firstname <> 'aaron'
-	-- <> does not equal
-	-- != will work as well
+select stateprovinceid, 
+	city, 
+	count(*) as record_count 
+from person.[address]
+group by stateprovinceid, city
+order by stateprovinceid, city
 
-	/* Ex 2 */
-	select * 
-	from adventureworks2017.person.person
-	where emailpromotion != 2
+select 
+	city, 
+	count(*) as record_count 
+from person.[address]
+group by city
+--order by --complete
 
-	/* Ex 3 */
-	select * 
-	from adventureworks2017.person.person
-	where emailpromotion = 2
-	and not (firstname = 'michael' or firstname = 'mark') 
-	-- note the parentheses around the OR logic
-	-- NOT before the or logic negates it
-
-	select * 
-	from adventureworks2017.person.person
-	where emailpromotion = 2
-	and firstname not in ('michael', 'mark')
+--complete #3 in class
 
 
 
 
+/* Filtering aggregated records.
 
-/* WHERE with comparison logic.
-
-1. How many records in the [Person].[Person] table have a BusinessEntityID greater than 1000?
-2. How many records in the [Person].[Person] table were modified on or after January 1, 2014?
-3. How many records in the [Person].[Person] table were modified in 1Q2014?
-4. How many records in the [Person].[Person] table were not modified in 1Q2014? Order by last name in ascending order.
+1. How many records exist for each StateProvinceID and City? Exlude State/City Sort by StateProvinceID, then City in ascending order.
+2. What value of City has the most number of records?
+3. What value of StateProvinceID and City has the most number of records?
 
 */
-
-	/* Ex 1 */
-	select * 
-	from adventureworks2017.person.person
-	where businessentityid > 1000
-
-	/* Ex 2 */
-	select * 
-	from adventureworks2017.person.person
-	where modifieddate >= '1/1/2014'
-	--note: dates use single quotations as well
-
-	/* Ex 3 */
-	select * 
-	from adventureworks2017.person.person
-	where modifieddate >= '1/1/2014'
-	and modifieddate < '4/1/2014'
-
-	select * 
-	from adventureworks2017.person.person
-	where modifieddate between '1/1/2014' and '3/31/2014'
-	--between works will with dates
-
-	/* Ex 4 */
-	--Complete with class participation.
