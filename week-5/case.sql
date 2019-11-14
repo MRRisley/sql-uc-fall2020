27e32dfc3cf195c86f1bcf4eea1e6d45d3f54f0d
Matt Risley <35645770+MRRisley@users.noreply.github.com>
2019-11-14 15:02:53 -0500

    Update case.sql

--- week-5/case.sql
+++ week-5/case.sql
@@ -16,6 +16,9 @@ Table: humanresources.employee
 	field is greater than or equal to 2 weeks (80 days) or less than. Include the 
 	BusinessEntityID field (as an employee ID #) as well as the original VacationHours field.
 	Order results by most vacation remaining to least.
+2. Using the logic from query 1, count the number of employees that fall within each bucket.
+3. Examine the query in #1. What would happen if there were NULL values in the field VacationHours?
+	Any ideas on how to avoid this?
 
 */
 
@@ -30,23 +33,59 @@ Table: humanresources.employee
 	order by vacationhours desc
 	
 	
+	/* Ex 2 */
+	select 
+	case when vacationhours >= 80 then 'GTE 2 wks'
+		else 'LT 2 wks' end as [wks_vacation],
+	count(*) as [count]
+	from humanresources.employee
+	group by case when vacationhours >= 80 then 'GTE 2 wks'
+		else 'LT 2 wks' end --no alias
+	--Why must we drop BusinessEntityID and VacationHours from the SELECT list?
 	
-/* isnull()
+	
+	/* Ex 3 */
+	--Answer in class.
+
+
+
+
+
+/* Multi-value CASE
 
 Table: person.person
 
-1. Count the number of records for each value in the field Title. 
-	Use the isnull() function to return the value 'unknown' for NULL.
+1. Using the field vacationhours, return a column that classifies whether the value in the
+	field is greater than or equal to 2 weeks (80 days) less than 80 days, or other. Include the 
+	BusinessEntityID field (as an employee ID #) as well as the original VacationHours field.
+	Order results by most vacation remaining to least.
 
 */
 
 	/* Ex 1 */
-	select
-	isnull(title, 'unknown'),
-	count(*) as record_count
-	from person.person
-	group by isnull(title, 'unknown')
-
+	select 
+	businessentityid as [emp_id],
+	vacationhours,
+	case when vacationhours >= 80 then 'GTE 2 wks'
+		when vacation hours < 90 then 'LT 2 wks' 
+		else 'other' end as [wks_vacation]
+	from humanresources.employee
+	order by vacationhours desc
+	--Mind the NULL! Note this is the safest way to treat your data.
+	--Know your ELSE! Make sure you're sure you account for all possibilities.
+	
+	/* Ex 1 */
+	select 
+	businessentityid as [emp_id],
+	vacationhours,
+	case when vacationhours >= 80 then 'GTE 2 wks'
+		when vacation hours < 90 then 'LT 2 wks' 
+		else 'other' end as [wks_vacation]
+	from humanresources.employee
+	order by vacationhours desc
+	--Mind the NULL! Note this is the safest way to treat your data.
+	--Know your ELSE! Make sure you're sure you account for all possibilities.	
+	
 
 
 
